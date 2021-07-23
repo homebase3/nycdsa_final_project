18649f0e09a68fb2c0e11c304cb13af138f5cf3e
homebase3 <65870950+homebase3@users.noreply.github.com>
2021-07-23 04:25:33 -0700

    added county health and demographics data.

--- Preprocess_geocode.R
+++ Preprocess_geocode.R
@@ -26,4 +26,36 @@ colnames(weather_geocodes) <- c("Key", long, lat)
 weather_geocodes %>% 
   write.csv(., "weather_geocodes.csv")
 
+# Geocode for cost_of_living cities
+cost_of_living_index <- read_csv("data/cost of living/advisorsmith_cost_of_living_index.csv")
+cost_of_living_index %<>% 
+  mutate(submit = paste0(City, ", ", State, ", United States"))
 
+### Expensive code
+out <- sapply(cost_of_living_index$submit, mb_geocode)
+###
+
+
+t(out) %>%
+  as.data.frame(.) %>% 
+  mutate(submit = row.names(.)) %>% 
+  left_join(cost_of_living_index,.) %>% 
+  select(submit, V1, V2)-> cost_of_living_index_geocodes
+
+cost_of_living_index_geocodes %>% 
+  write.csv(., "cost_of_living_index_geocodes.csv")
+
+# Add in LGBT MSA translation
+lgbt_data <- read_excel("data/population/lgbt_data.xlsx", 
+                        sheet = "Sheet2")
+MSAs <- FIPS_to_MSA_2020['CBSA Title'] %>% 
+  mutate(State = str_sub(`CBSA Title`, -2, -1)) %>% 
+  unique(.) %>% 
+  drop_na(.)
+
+lgbt_data$percent_key <- as.data.frame(str_locate(lgbt_data$Data,"%"))$start
+lgbt_data %>% 
+  mutate(`% LGBT` = as.numeric(str_sub(Data,percent_key - 3, percent_key -1))/100) %>% 
+  mutate(MSA = str_sub(Data,1, percent_key -4)) %>% 
+  mutate(State = str_sub(MSA, -3, -2)) %>% 
+  write.csv('lgbt_data_with_MSA.csv')
