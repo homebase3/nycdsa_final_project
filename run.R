55ae6af3c339250d2a0a6ffc4a414599fc97c3b1
homebase3 <65870950+homebase3@users.noreply.github.com>
2021-08-06 22:55:53 -0700

    fix run.R

--- run.R
+++ run.R
@@ -2,6 +2,7 @@ library(shiny)
 library(rmarkdown)
 port <- Sys.getenv('PORT')
 rmarkdown::run(
+  file = "app.Rmd",
   dir = getwd(),
   shiny_args = list(host = '0.0.0.0',port = as.numeric(port))
 )
